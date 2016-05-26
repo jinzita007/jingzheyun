67f43c69310b563c29e6f99657fc6b63bdd120da
zhou <jinzita007@yahoo.com>
2016-05-26 17:49:10 +0800

    0.0.3

---
+++ js/bottom.js
@@ -0,0 +1,14 @@
+//首页------划过效果
+$(function(){
+	
+$(".hover-ewm").hover(function(){
+		$(this).children("div").addClass("dis")
+	},function(){
+		$(this).children("div").removeClass("dis")
+	})	
+	
+	
+	
+	//$(".titile2").text("qiye&nbsp;xinwen")
+})
+
